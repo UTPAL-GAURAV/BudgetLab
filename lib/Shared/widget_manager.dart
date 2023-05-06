9d2a08d0e8111b423cda0beb5b2c6d441954e43f
Utpal Gaurav <utpal.gaurav@sap.com>
2023-05-06 23:00:24 +0530

    Bottom Navigation Bar created

--- lib/Shared/widget_manager.dart
+++ lib/Shared/widget_manager.dart
@@ -20,3 +20,30 @@ ThemeData getButtonTheme1() {
 /**
  * Calendars
  */
+
+
+/**
+ * Bottom Navigation Bar
+ */
+
+getBottomNavigationBar() {
+  return BottomNavigationBar(
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.add_circle_outline),
+        label: 'Business',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'School',
+      ),
+    ],
+    // currentIndex: _selectedIndex,
+    selectedItemColor: Colors.amber[800],
+    // onTap: _onItemTapped,
+  );
+}
